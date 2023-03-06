5d4d387dd028237a714050f56aeba4e4f6e9cbc0
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-03-06 15:47:48 -0500

    shout out to todd

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,15 @@
 import functions from "firebase-functions"
 import express from "express"
 import cors from "cors"
-import { addBlog, deleteBlog, getBlogs, updateBlog } from "./src/blogFunctions.js"
+import { addBlog, deleteBlog, getBlogs, updateBlog } from "./src/BlogFunctions.js"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get("/", (req, res) => res.send("This works!!!"))
-
 app.get("/blog", getBlogs)
 app.post("/blog", addBlog)
-app.patch("/blog/:blodId", updateBlog)
+app.patch("/blog/:blogId", updateBlog)
 app.delete("/blog/:blogId", deleteBlog)
 
 export const api = functions.https.onRequest(app)
